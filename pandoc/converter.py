21c5fc6eb1205ed68739d5a1764465ada6a81f65
João Fernandes <joaofilho8274@gmail.com>
2024-12-10 11:50:02 -0300

    Converte vários arquivos, mas não mantém as pastas

--- pandoc/converter.py
+++ pandoc/converter.py
@@ -4,8 +4,19 @@ import os
 pandoc_exe = "pandoc.exe"
 
 def exec_pandoc(md_file, dist_path):
+    #relative_path = os.path.relpath(os.path.abspath(md_file), "../docs")
+    #dist_path = os.path.join("../docs", relative_path)
     html_file = os.path.join(dist_path, os.path.basename(md_file).replace(".md", ".html"))
     subprocess.run([pandoc_exe, md_file, "-o", html_file, "--template", "template.html"])
     return None
 
-exec_pandoc("../docs/teste.md", "../dist")
+def convert_md_files(docs, dist):
+    for path, _, files in os.walk(docs):
+        for file in files:
+            if file.endswith(".md"):
+                exec_pandoc(os.path.join(path, file), dist)
+
+def main(docs, dist):
+    convert_md_files(docs, dist)
+
+main("../docs", "../dist")
